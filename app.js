844321ca9d0d0643b4d7692b223c05257df960e7
maxime <maxime.devos59@hotmail.fr>
2022-10-12 15:18:40 +0200

    initialise getComputerChoice

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("hello world!");
+const choices = ["pierre", "papier", "ciseaux"];
+
+function getComputerChoice() {
+  const index = Math.floor(Math.random() * choices.length);
+  return choices[index];
+}
+
+console.log(getComputerChoice());
