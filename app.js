031f60eb0a46261421b4be97b2d298ea7300fe85
maxime <maxime.devos59@hotmail.fr>
2022-10-12 16:36:23 +0200

    include playRound

--- app.js
+++ app.js
@@ -5,4 +5,28 @@ function getComputerChoice() {
   return choices[index];
 }
 
-console.log(getComputerChoice());
+let playerSelection = prompt("Pierre papier ou ciseaux?");
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    console.log("égalité");
+  } else if (
+    (playerSelection === "pierre" && computerSelection === "ciseaux") ||
+    (playerSelection === "ciseaux" && computerSelection === "papier") ||
+    (playerSelection === "papier" && computerSelection === "pierre")
+  ) {
+    console.log("Vous avez gagné!");
+  } else {
+    console.log("Vous avez perdu!");
+  }
+}
+
+console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+  while (playRound < 5) {
+    playRound++;
+  }
+}
